764642c2d681a4d9836398588f1164cc32d2a6d3
SatyamPandey <70374097+sp-sketch@users.noreply.github.com>
2021-05-30 16:41:04 +0530

    Add files via upload

---
+++ db/quest.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    name: `MrSon's Froakies MISSING`,
+    num: 0,
+    reward: [
+      500,
+      "mewtwo"
+    ],
+    req: "25 froakie catch",
+    start: {
+      name: "MrSon || Froakie Lover",
+      av: "https://cdn.discordapp.com/attachments/720215392491929620/724598479476883517/e41a2738f3f20d7c5342a119d1d0246e.png",
+      text: "Hey ;-; I am MrSon. SOMEONE STOLE ALL MY FROAKIES. You're saying I have too many? I LIKE TO COLLECT EM. \n Wait.... Why not i Give you something to do? Catch 25 froakies and I will get up to u in your dm to give you 500 credits & Mewtwo :)! Bye!",
+      end: "OMG YOU GOT EM YAY. CHECK YOUR POKEMON LIST FOR THAT BEAUTY MEWTWO and check your balance for your credits. Thanks for catching em ^=^."
+    }
+  },
+  {
+    name: `Mr. naman rares missing`,
+    num: 1,
+    reward: [
+      "shaymin",
+      "victini"
+    ],
+    req: 'catch a legendary pokemon',
+    start: {
+      name: `Mr. Naman`,
+      av: 'https://cdn.discordapp.com/attachments/718471742145953943/725640151828660295/349-3498024_pokemon-trainer-red-manga-sprite.png',
+      text: 'Heya pokemon trainers!! my legendary pokemon escaped,would you help me find it!! If you help me to find them i will give you a little reward to find them ofcourse its contains some credits nd a rare pokemon',
+      end: `Welcome back seems like you found my precious legendary pokemon!! So now its my turn to give you the rewards. Congratulations you've earned a shaymin and victini`
+    }
+  }
+]
---
+++ db/shadow.js
@@ -0,0 +1,150 @@
+module.exports = [
+  {
+    name: "mewtwo",
+    type: "Psychic | Dark",
+    url: "https://i.imgur.com/MqNFAzN.png",
+    hp: 106,
+    atk: 110,
+    def: 90,
+    spatk: 154,
+    spdef: 90,
+    speed: 130
+  },
+  {
+    name: "lugia",
+    type: "Psychic | Flying",
+    url: "https://i.imgur.com/Lm5capk.png",
+    hp: 106,
+    atk: 90,
+    def: 130,
+    spatk: 90,
+    spdef: 154,
+    speed: 110
+  },
+  {
+    name: "celebi",
+    type: "Psychic | Grass",
+    url: "https://imgur.com/5R6fc2j.png",
+    hp: 100,
+    atk: 100,
+    def: 100,
+    spatk: 100,
+    spdef: 100,
+    speed: 100
+  },
+  {
+    name: "mew",
+    type: "Psychic | Ghost",
+    url: "https://imgur.com/hEz19J2.png",
+    hp: 100,
+    atk: 100,
+    def: 100,
+    spatk: 100,
+    spdef: 100,
+    speed: 100
+  },
+  {
+    name: "solgaleo",
+    type: "Psychic | Steel",
+    url: "https://i.imgur.com/ww7RPnp.png",
+    hp: 137,
+    atk: 137,
+    def: 107,
+    spatk: 113,
+    spdef: 89,
+    speed: 97
+  }
+
+]
+
+
+
+// {
+//     name: "bulbasaur",
+//     type: "Shadow",
+//     url: "https://images-ext-2.discordapp.net/external/f-NpTVoG_2pUjCEzjepoSuDG2f0EwWaKKQ7awskr-CU/https/i.imgur.com/lWjIiLj.png?width=517&height=517",
+//     hp: 49,
+//     atk: 59,
+//     def: 39,
+//     spatk: 78,
+//     spdef: 52,
+//     speed: 49
+//   },
+//   {
+//     name: "charmander",
+//     type: "Shadow",
+//     url: "https://images-ext-2.discordapp.net/external/_uuM5aOUObsoyjfB05yyqbVhqWKYdrDv2pbaKfwl9rc/https/i.imgur.com/rooi0Rk.png?width=517&height=517",
+//     hp: 42,
+//     atk: 62,
+//     def: 35,
+//     spatk: 72,
+//     spdef: 40,
+//     speed: 71
+//   },
+//   {
+//     name: "squirtle",
+//     type: "Shadow",
+//     url: "https://images-ext-1.discordapp.net/external/P7O6K1m94NorVBCABrBhx8qjVJjnn1XAfzgvlmxUV10/https/i.imgur.com/84VD8E8.png?width=517&height=517",
+//     hp: 49,
+//     atk: 57,
+//     def: 52,
+//     spatk: 60,
+//     spdef: 52,
+//     speed: 47
+//   },
+//   {
+//     name: "chikorita",
+//     type: "Shadow",
+//     url: "https://images-ext-2.discordapp.net/external/g3EO0GUiovzRzCpdVLx__fa45AA7rMKD7hHnhDsLdQg/https/i.imgur.com/bcWdI2R.png?width=517&height=517",
+//     hp: 49,
+//     atk: 59,
+//     def: 52,
+//     spatk: 59,
+//     spdef: 52,
+//     speed: 49
+//   },
+//   {
+//     name: "cyndaquil",
+//     type: "Shadow",
+//     url: "https://images-ext-2.discordapp.net/external/OlBqlCzUet7eEZQlJfna2TEjqy-URtYrgzFau5pMzto/https/i.imgur.com/RTbqIvF.png?width=517&height=517",
+//     hp: 43,
+//     atk: 62,
+//     def: 35,
+//     spatk: 72,
+//     spdef: 40,
+//     speed: 71
+//   },
+//   {
+//     name: "totodile",
+//     type: "Shadow",
+//     url: "https://images-ext-1.discordapp.net/external/P7f-aMG714HS54kxzwznVMss1Mp9oXsX2fehgbKG-cA/https/i.imgur.com/rrHUuxl.png?width=517&height=517",
+//     hp: 55,
+//     atk: 78,
+//     def: 54,
+//     spatk: 52,
+//     spdef: 39,
+//     speed: 47
+//   }
+
+  // {
+  //   name: "buneary",
+  //   type: "Normal | Dark",
+  //   url: "https://imgur.com/ORTdXZD.png",
+  //   hp: 55,
+  //   atk: 66,
+  //   def: 44,
+  //   spatk: 44,
+  //   spdef: 56,
+  //   speed: 85
+  // },
+  // {
+  //   name: "lopunny",
+  //   type: "Normal | Dark",
+  //   url: "https://imgur.com/OgkGznO.png",
+  //   hp: 65,
+  //   atk: 76,
+  //   def: 84,
+  //   spatk: 54,
+  //   spdef: 96,
+  //   speed: 105
+  // },
