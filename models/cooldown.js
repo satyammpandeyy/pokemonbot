8dbe39c833c48807edec3415d79ba9b3debb1a12
SatyamPandey <70374097+sp-sketch@users.noreply.github.com>
2021-07-31 08:08:12 +0530

    Delete models directory

--- models/cooldown.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const productSchema = mongoose.Schema({
-    id: {type:String, required: true},
-    time: {type:Number, required:true}
-});
-
-module.exports = mongoose.model("Cooldown", productSchema);
